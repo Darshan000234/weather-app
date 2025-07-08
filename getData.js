83c9c0f0547cbe8d7aada6ddfdbcfbd78ab68497
Darshan000234 <145257885+Darshan000234@users.noreply.github.com>
2025-07-08 18:06:12 +0530

    Fix: updated API URL to use HTTPS

--- getData.js
+++ getData.js
@@ -1,5 +1,5 @@
 export async function getData(city_name) {
-    let res = await fetch(`http://api.weatherapi.com/v1/current.json?key=5dfcd0bf0f1744d5b8d122432250107&q=${city_name}&aqi=no`);
+    let res = await fetch(`https://api.weatherapi.com/v1/current.json?key=5dfcd0bf0f1744d5b8d122432250107&q=${city_name}&aqi=no`);
     let data = await res.json();
     return data;
 }
